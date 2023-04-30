f5fd3cf49c3385a3b74faf0ed5f56e76c6015e1d
Shubham Dhatwalia <122049013+ShubhamDhatwalia@users.noreply.github.com>
2023-04-30 16:30:31 +0530

    Time: 1 ms (99.41%), Space: 43.4 MB (24.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,33 +1,33 @@
 //  Approach 1 ::::::    TC:O(3N) = O(N)  SC:O(N)
 
 
-class Solution {
-    public int trap(int[] arr) {
-        int n = arr.length;
+// class Solution {
+//     public int trap(int[] arr) {
+//         int n = arr.length;
         
-        int lmax [] = new int[n];
-		lmax[0] = arr[0];
+//         int lmax [] = new int[n];
+// 		lmax[0] = arr[0];
         
-		for(int i=1; i<n; i++){
-			lmax[i] = Math.max(lmax[i-1], arr[i]);
-		}
-		int rmax [] = new int[n];
-		rmax[n-1] = arr[n-1];
+// 		for(int i=1; i<n; i++){
+// 			lmax[i] = Math.max(lmax[i-1], arr[i]);
+// 		}
+// 		int rmax [] = new int[n];
+// 		rmax[n-1] = arr[n-1];
         
-		for(int i=n-2; i>=0; i--){
-			rmax[i] = Math.max(rmax[i+1], arr[i]);
-		}
-        int totalwater = 0;
+// 		for(int i=n-2; i>=0; i--){
+// 			rmax[i] = Math.max(rmax[i+1], arr[i]);
+// 		}
+//         int totalwater = 0;
 		
-		for(int i=0; i<n; i++){
-			int maxheight = Math.min(lmax[i], rmax[i]);
-			int waterheight = maxheight - arr[i];
-			int currUnitOfWater = waterheight * 1;
-			totalwater += currUnitOfWater; 
-		}
-        return totalwater;
-    }
-}
+// 		for(int i=0; i<n; i++){
+// 			int maxheight = Math.min(lmax[i], rmax[i]);
+// 			int waterheight = maxheight - arr[i];
+// 			int currUnitOfWater = waterheight * 1;
+// 			totalwater += currUnitOfWater; 
+// 		}
+//         return totalwater;
+//     }
+// }
 
 
 
@@ -61,33 +61,33 @@ class Solution {
 
 // Approach 3 ::::      TC:O(N)   SC:O(1)
 
-// class Solution {
-//     public int trap(int[] arr) {
-//         int n = arr.length;
-//         int lb = arr[0];
-//         int rb = arr[n-1];
-//         int l =1;
-//         int r = n-2;
-//         int totalWater =0;
+class Solution {
+    public int trap(int[] arr) {
+        int n = arr.length;
+        int lb = arr[0];
+        int rb = arr[n-1];
+        int l =1;
+        int r = n-2;
+        int totalWater =0;
 
-//         while(l<=r){
-//             if(lb<=rb){
-//                 if(lb < arr[l]){
-//                     lb = arr[l];
-//                 }else{
-//                     totalWater += lb-arr[l];
-//                 }
-//                 l++;
-//             }
-//             else{
-//                 if(rb < arr[r]){
-//                     rb = arr[r];
-//                 }else{
-//                     totalWater += rb-arr[r];
-//                 }
-//                 r--;
-//             }
-//         }
-//         return totalWater;
-//     }
-// }
+        while(l<=r){
+            if(lb<=rb){
+                if(lb < arr[l]){
+                    lb = arr[l];
+                }else{
+                    totalWater += lb-arr[l];
+                }
+                l++;
+            }
+            else{
+                if(rb < arr[r]){
+                    rb = arr[r];
+                }else{
+                    totalWater += rb-arr[r];
+                }
+                r--;
+            }
+        }
+        return totalWater;
+    }
+}
