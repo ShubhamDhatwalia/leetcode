e017472fbc4146e466abe588eb68bf4c7f3823d0
Shubham Dhatwalia <122049013+ShubhamDhatwalia@users.noreply.github.com>
2023-05-15 17:11:51 +0530

    Time: 26 ms (53.56%), Space: 43.2 MB (34.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,23 +1,53 @@
+// Approach 1   TC:: O(N^3)
+
+// class Solution {
+//     public String longestPalindrome(String s) {
+//         if(isPalindrome(s)) return s;
+        
+//         for(int i = s.length()-1; i>=0; i-- ){
+//             for(int j=0; i+j<=s.length(); j++){
+//                 if(isPalindrome(s.substring(j , i+j))){
+//                     return s.substring(j, i+j);
+//                 }
+//             }
+//         }
+//         return "";
+//     }
+//     public boolean isPalindrome(String s){
+//         int l=0;
+//         int r= s.length()-1;
+//         while(l<=r && s.charAt(l) == s.charAt(r)){
+//             l++;
+//             r--;
+//         }
+//         return l>=r;
+//     }
+// }
+
+// Approach 2          TC:: O(N^2)
+
 class Solution {
     public String longestPalindrome(String s) {
-        if(isPalindrome(s)) return s;
+        String lps ="";
         
-        for(int i = s.length()-1; i>=0; i-- ){
-            for(int j=0; i+j<=s.length(); j++){
-                if(isPalindrome(s.substring(j , i+j))){
-                    return s.substring(j, i+j);
-                }
+        for(int i=0; i<s.length(); i++){
+            String odd = expandPalindrome(s , i, i);
+            String even = expandPalindrome(s, i, i+1);
+            
+            if(lps.length() < odd.length()){
+                lps = odd;
+            }
+            if(lps.length() < even.length()){
+                lps = even;
             }
         }
-        return "";
+        return lps;
     }
-    public boolean isPalindrome(String s){
-        int l=0;
-        int r= s.length()-1;
-        while(l<=r && s.charAt(l) == s.charAt(r)){
-            l++;
-            r--;
+    public String expandPalindrome(String s, int l, int r){
+        while(l >=0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+            l--;
+            r++;
         }
-        return l>=r;
+        return s.substring(l+1, r);
     }
 }
