ed6d7b799a593c6200f5e819a43aa53557ae7c17
Shubham Dhatwalia <122049013+ShubhamDhatwalia@users.noreply.github.com>
2023-06-08 16:56:00 +0530

    Time: 2 ms (20.63%), Space: 43.3 MB (31.12%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,40 +13,74 @@
  *     }
  * }
  */
-class Solution {
-    class Pair{
-        boolean isBST;
-        long min;
-        long max;
+// Approach 1:
+
+
+// class Solution {
+//     class Pair{
+//         boolean isBST;
+//         long min;
+//         long max;
         
-        Pair(boolean isBST, long min, long max){
-            this.isBST = isBST;
-            this.min = min;
-            this.max = max;
-        }
-    }
+//         Pair(boolean isBST, long min, long max){
+//             this.isBST = isBST;
+//             this.min = min;
+//             this.max = max;
+//         }
+//     }
         
-        Pair dfs(TreeNode node){
-            if(node == null){
-                return new Pair(true,Long.MAX_VALUE, Long.MIN_VALUE);
-            }
-            Pair left = dfs(node.left);
-            Pair right = dfs(node.right);
+//         Pair dfs(TreeNode node){
+//             if(node == null){
+//                 return new Pair(true,Long.MAX_VALUE, Long.MIN_VALUE);
+//             }
+//             Pair left = dfs(node.left);
+//             Pair right = dfs(node.right);
             
-            boolean amIBST = left.isBST && right.isBST && node.val > left.max && node.val < right.min;
+//             boolean amIBST = left.isBST && right.isBST && node.val > left.max && node.val < right.min;
             
-            if(amIBST == false){
-                return new Pair(false, -1, -1);
-            }
+//             if(amIBST == false){
+//                 return new Pair(false, -1, -1);
+//             }
             
-            long min = Math.min(node.val, left.min);
-            long max = Math.max(node.val, right.max);
-            return new Pair(true, min, max);
-        }
+//             long min = Math.min(node.val, left.min);
+//             long max = Math.max(node.val, right.max);
+//             return new Pair(true, min, max);
+//         }
         
     
+//     public boolean isValidBST(TreeNode root) {
+//         Pair res = dfs(root);
+//         return res.isBST;
+//     }
+// }
+
+
+// Approach 2:::
+
+
+class Solution {
     public boolean isValidBST(TreeNode root) {
-        Pair res = dfs(root);
-        return res.isBST;
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        inOrder(root, ans);
+        
+        boolean valid = true;
+        int prev = ans.get(0);
+        
+        for(int i=1; i< ans.size(); i++){
+            if(ans.get(i) <= prev){
+                valid = false;
+            }
+            prev = ans.get(i);
+        }
+        return valid;
+    }
+    public void inOrder(TreeNode node, ArrayList<Integer> ans){
+        if(node == null) return;
+        
+        inOrder(node.left, ans);
+        ans.add(node.val);
+        inOrder(node.right, ans);
     }
 }
