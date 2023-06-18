5cdde87e026f7580d1b01b15bcb0926a10e7b5cc
Shubham Dhatwalia <122049013+ShubhamDhatwalia@users.noreply.github.com>
2023-06-19 00:20:59 +0530

    Time: 138 ms (44.12%), Space: 64.9 MB (85.17%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -6,31 +6,27 @@ class NumMatrix {
         int n = matrix.length;
         int m = matrix[0].length;
         
-        pref = new int[n+1][m+1];
-        for(int i=1; i<=n; i++)
-        {
-            for(int j =1; j<=m; j++)
-            {
-//                 int A = matrix[i][j];
-//                 int B = j==0 ? 0 : matrix[i][j-1];
-//                 int C = i==0 ? 0 : matrix[i-1][j];
-//                 int D = i==0 || j==0 ? 0 : matrix[i-1][j-1];
+        pref = new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j =0; j<m; j++){
+                int A = matrix[i][j];
+                int B = j==0 ? 0 : pref[i][j-1];
+                int C = i==0 ? 0 : pref[i-1][j];
+                int D = i==0 || j==0 ? 0 : pref[i-1][j-1];
                 
-//                 pref[i][j] = A+B+C-D;
-                pref[i][j]=pref[i-1][j]+pref[i][j-1]-pref[i-1][j-1]+matrix[i-1][j-1];
+                pref[i][j] = A+B+C-D;
             }
         }
         
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-//         int A = pref[row2][col2];
-//         int B = col1==0 ? 0 : pref[row2][col1-1];
-//         int C = row1==0 ? 0 : pref[row1-1][col2];
-//         int D = row1==0 || col1==0 ? 0 : pref[row1-1][col1-1];
+        int A = pref[row2][col2];
+        int B = col1==0 ? 0 : pref[row2][col1-1];
+        int C = row1==0 ? 0 : pref[row1-1][col2];
+        int D = row1==0 || col1==0 ? 0 : pref[row1-1][col1-1];
         
-//         return A-B-C+D;
-        return pref[row2+1][col2+1]-pref[row2+1][col1]-pref[row1][col2+1]+pref[row1][col1];
+        return A-B-C+D;
     }
 }
 
