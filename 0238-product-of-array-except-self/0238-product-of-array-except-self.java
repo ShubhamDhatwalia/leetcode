d1b8292a504d5ccc5ae7cb4c77c51e599b5e9b69
Shubham Dhatwalia <122049013+ShubhamDhatwalia@users.noreply.github.com>
2023-06-20 16:53:08 +0530

    Time: 1 ms (100.00%), Space: 53.1 MB (38.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,21 +1,47 @@
+//   TC:  O(N)      SC: O(N)
+
+
+// class Solution {
+//     public int[] productExceptSelf(int[] nums) {
+//         int ans[] = new int[nums.length];
+//         int pref [] = new int[nums.length];
+//         pref[0] = 1;
+        
+//         for(int i=1; i< nums.length; i++){
+//             pref[i] = pref[i-1] * nums[i-1];
+//         }
+//         int sufix[] = new int[nums.length];
+//         sufix[nums.length-1] = 1;
+//         for(int i=nums.length-2; i>=0; i--){
+//             sufix[i] = sufix[i+1] * nums[i+1];
+//         }
+        
+//         for(int i=0; i<nums.length; i++){
+//             ans[i] = pref[i]*sufix[i];
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+//     TC: O(N)    SC:(1)
+
 class Solution {
+    
     public int[] productExceptSelf(int[] nums) {
-        // int ans[] = new int[nums.length];
-        int pref [] = new int[nums.length];
-        pref[0] = 1;
+        
+        int ans[] = new int[nums.length];
+        ans[0] = 1;
         
         for(int i=1; i< nums.length; i++){
-            pref[i] = pref[i-1] * nums[i-1];
+            ans[i] = ans[i-1] * nums[i-1];
         }
-        int sufix[] = new int[nums.length];
-        sufix[nums.length-1] = 1;
-        for(int i=nums.length-2; i>=0; i--){
-            sufix[i] = sufix[i+1] * nums[i+1];
+        int sufix =1;
+        for(int i=nums.length-1; i>=0; i--){
+            ans[i] *= sufix;
+            sufix *= nums[i];
         }
-        
-        for(int i=0; i<nums.length; i++){
-            nums[i] = pref[i]*sufix[i];
-        }
-        return nums;
+        return ans;
     }
 }
