db7be46c07855e1d3ec8fa9e25834c9df159358c
Shubham Dhatwalia <122049013+ShubhamDhatwalia@users.noreply.github.com>
2023-06-26 18:07:01 +0530

    Time: 46 ms (38.68%), Space: 55.8 MB (66.76%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<arr.length; i++){
+            
+            int rem = (arr[i])%k;
+            if(rem <0) rem = rem+k;
+            map.put(rem, map.getOrDefault(rem, 0)+1);
+        }
+        
+        for(int rem : map.keySet()){
+            if(rem ==0){
+                if(map.containsKey(0) && map.get(0)%2 !=0) return false;
+               continue;
+            }
+            
+            int comprem = k-rem;
+            
+            if(map.containsKey(comprem) == false) {
+                return false;
+                
+            }
+            
+            // if(map.get(rem) != map.get(comprem)) return false;
+            int a = map.get(rem);
+            int b = map.get(comprem);
+            if(a != b) return false;
+        }
+        return true;
+    }
+}
