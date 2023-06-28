f9c06666b10ce031cb370900cc47c7dc0738838a
Shubham Dhatwalia <122049013+ShubhamDhatwalia@users.noreply.github.com>
2023-06-29 00:50:15 +0530

    Time: 53 ms (45.84%), Space: 58.5 MB (5.30%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,61 +1,63 @@
 class Solution {
-    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-        // Adjacency list
-        List<List<Pair<Integer, Double>>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+    class Edge{
+        int nbr;
+        double wt;
+        Edge(int nbr, double wt){
+            this.nbr = nbr;
+            this.wt = wt;
         }
-
-        for (int i = 0; i < edges.length; i++) {
+    }
+    class Pair{
+        int node;
+        double wsf;
+        Pair(int node, double wsf){
+            this.node = node;
+            this.wsf = wsf;
+        }
+    }
+    
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        
+        ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
+        
+        for(int i=0; i<n; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int i=0; i<edges.length; i++){
             int u = edges[i][0];
             int v = edges[i][1];
-            double p = succProb[i];
-            adj.get(u).add(new Pair<>(v, p));
-            adj.get(v).add(new Pair<>(u, p));
+            double wt = succProb[i];
+            
+            graph.get(u).add(new Edge(v, wt));
+            graph.get(v).add(new Edge(u, wt));
         }
-
-        // Distances array
-        double[] dist = new double[n];
-        Arrays.fill(dist, 0.0);
-        dist[start] = 1.0;
-
-        // Queue for BFS
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-
-            for (Pair<Integer, Double> pair : adj.get(curr)) {
-                int node = pair.getKey();
-                double prob = pair.getValue();
-                double newProb = dist[curr] * prob;
-
-                if (newProb > dist[node]) {
-                    dist[node] = newProb;
-                    queue.offer(node);
-                }
+        
+        boolean vis[] = new boolean[n];
+        double dis[] = new double[n];
+        Arrays.fill(dis,0.0);
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->{return Double.compare(b.wsf,a.wsf);});
+        
+        pq.add(new Pair(start, 1));
+        
+        while(pq.size() >0){
+            Pair curr = pq.remove();
+            int node = curr.node;
+            double wsf = curr.wsf;
+            
+            if(vis[node] ) continue;
+            vis[node] = true;
+            
+            dis[node] = wsf;
+            
+            for(Edge e: graph.get(node)){
+                int nbr = e.nbr;
+                double wt = e.wt;
+                
+                if(!vis[nbr]) pq.add(new Pair(nbr, wsf*wt));
             }
         }
-
-        return dist[end];
-    }
-
-    private class Pair<K, V> {
-        private K key;
-        private V value;
-
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
+        return dis[end];
     }
 }
