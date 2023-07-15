69fd9023fd71d694729229c480be99fc8c559ddc
Shubham Dhatwalia <122049013+ShubhamDhatwalia@users.noreply.github.com>
2023-07-16 00:59:01 +0530

    Time: 6 ms (58.57%), Space: 57.2 MB (68.92%) - LeetHub

--- 2017-grid-game/2017-grid-game.java
+++ 2017-grid-game/2017-grid-game.java
@@ -2,32 +2,25 @@ class Solution {
     public long gridGame(int[][] grid) {
         int n =2;
         int m = grid[0].length;
-        long sufix[] = new long[m];
-         long pref[] = new long[m];
-        
+        long arr[][] = new long[n][m];
+        arr[0][m-1] = grid[0][m-1];
+        arr[1][0] = grid[1][0];
         for(int i=m-2; i>=0; i--){
-            sufix[i] = grid[1][i] + sufix[i+1];
+            arr[0][i] = grid[0][i] + arr[0][i+1];
         }
         
         for(int i=1; i<m; i++){
-            pref[i] = grid[0][i] + pref[i-1];
+            arr[1][i] = grid[1][i] + arr[1][i-1];
         }
         
-        long ans = Math.min(pref[m-1],sufix[0]);
+        long ans = Long.MAX_VALUE;
         
-//         for(int i=0; i<m; i++){
-//             long option1 = i+1 == m ?0 : sufix[i+1];
-//             long option2 = i-1 <0 ?0 : pref[i-1];
+        for(int i=0; i<m; i++){
+            long option1 = i+1 == m ?0 : arr[0][i+1];
+            long option2 = i-1 <0 ?0 : arr[1][i-1];
             
-//             long robot2Best = Math.max(option1, option2);
-//             ans = Math.min(ans, robot2Best);
-//         }
-        for(int i=1;i<m-1;i++)
-        {
-            long curr=Math.max(pref[m-1]-pref[i],sufix[0]-sufix[i]);
-            if(curr>0)
-                    ans=Math.min(ans,curr);
-            
+            long robot2Best = Math.max(option1, option2);
+            ans = Math.min(ans, robot2Best);
         }
         return ans;
     }
